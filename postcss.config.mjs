cfc5c8d341da8622a00786fc4667c72cd1d15995
Puneeth067 <kumarpuneeth631@gmail.com>
2025-05-30 22:27:36 +0530

    add ons final

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,9 @@
-// postcss.config.mjs
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
 
-export default config
+export default config;
